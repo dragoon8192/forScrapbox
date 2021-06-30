faac06c0636da7f95cab32c33dc1f5e70dd6be27
dragoon8192_mac <dragoon8192@gmail.com>
2021-06-30 16:30:36 +0900

    new:   TextInputEvent.js

---
+++ TextInputEvent.js
@@ -0,0 +1,30 @@
+import { scrapboxDOM } from "../UserScript:ScrapboxDOM/script.js";
+export class KeyboardEventManager {
+    #eventTarget;
+    constructor(eventTarget) {
+        this.#eventTarget = eventTarget;
+    }
+    keyDownUp(keyboardEventInit) {
+        const patch = {
+            bubbles: true,
+            cancelable: true,
+        };
+        const pKeyboardEventInit = Object.assign(Object.create(keyboardEventInit), patch);
+        const keyboardEventDown = new KeyboardEvent("keydown", pKeyboardEventInit);
+        const keyboardEventUp = new KeyboardEvent("keyup", pKeyboardEventInit);
+        this.#eventTarget.dispatchEvent(keyboardEventDown);
+        this.#eventTarget.dispatchEvent(keyboardEventUp);
+    }
+    async insertText(text, { wait = 1 } = {}) {
+        this.#eventTarget.focus();
+        this.#eventTarget.value = text;
+        const uiEvent = new UIEvent("input");
+        this.#eventTarget.dispatchEvent(uiEvent);
+        await this.sleep(wait);
+    }
+    sleep(msec) {
+        return(new Promise(resolve => setTimeout(resolve,msec)));
+    }
+}
+
+const keyboardEventManager = new KeyboardEventManager(scrapboxDOM.textInput);
